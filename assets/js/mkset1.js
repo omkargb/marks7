0fd885b7d82d4903ff0929f4457f0e56534c70d6
Omkar G B <omkargb107@gmail.com>
2021-05-02 20:47:07 +0530

    Add files via upload

---
+++ assets/js/mkset1.js
@@ -0,0 +1,76 @@
+﻿	function setmk1()
+		{
+			var mk=document.getElementById("marks1").value;
+			document.getElementById("mk1").innerHTML=mk;
+			console.log("mk = "+mk);
+			var m1 = $('.sa') ;	var m2 = $('.re') ;	var m3 = $('.ga') ;
+			var m4 = $('.ma') ;	var m5 = $('.pa') ;	var m6 = $('.dha') ;	var m7 = $('.ni') ;			
+			
+			var m11 = $('.sa1') ;	var m12 = $('.re1') ;	var m13 = $('.ga1') ;
+			var m14 = $('.ma1') ;	var m15 = $('.pa1') ;	var m16 = $('.dha1') ;	var m17 = $('.ni1') ;			
+			
+			if(mk=="1")
+			{
+				m1.removeClass().addClass('progress-bar sa1');		m12.removeClass().addClass('progress-bar re');
+				m13.removeClass().addClass('progress-bar ga');		m14.removeClass().addClass('progress-bar ma');
+				m15.removeClass().addClass('progress-bar pa');		m16.removeClass().addClass('progress-bar dha');
+				m17.removeClass().addClass('progress-bar ni');
+			}			
+			
+			else if(mk=="2")
+			{
+				m1.removeClass().addClass('progress-bar sa1');		m2.removeClass().addClass('progress-bar re1');
+				m13.removeClass().addClass('progress-bar ga');		m14.removeClass().addClass('progress-bar ma');
+				m15.removeClass().addClass('progress-bar pa');		m16.removeClass().addClass('progress-bar dha');
+				m17.removeClass().addClass('progress-bar ni');
+			}
+						
+			else if(mk=="3")
+			{
+				m1.removeClass().addClass('progress-bar sa1');		m2.removeClass().addClass('progress-bar re1');
+				m3.removeClass().addClass('progress-bar ga1');		m14.removeClass().addClass('progress-bar ma');
+				m15.removeClass().addClass('progress-bar pa');		m16.removeClass().addClass('progress-bar dha');
+				m17.removeClass().addClass('progress-bar ni');
+			}
+									
+			else if(mk=="4")
+			{
+				m1.removeClass().addClass('progress-bar sa1');		m2.removeClass().addClass('progress-bar re1');
+				m3.removeClass().addClass('progress-bar ga1');		m4.removeClass().addClass('progress-bar ma1');
+				m15.removeClass().addClass('progress-bar pa');		m16.removeClass().addClass('progress-bar dha');
+				m17.removeClass().addClass('progress-bar ni');
+			}			
+			
+			else if(mk=="5")
+			{
+				m1.removeClass().addClass('progress-bar sa1');		m2.removeClass().addClass('progress-bar re1');
+				m3.removeClass().addClass('progress-bar ga1');		m4.removeClass().addClass('progress-bar ma1');
+				m5.removeClass().addClass('progress-bar pa1');		m16.removeClass().addClass('progress-bar dha');
+				m17.removeClass().addClass('progress-bar ni');
+			}
+			
+			else if(mk=="6")
+			{
+				m1.removeClass().addClass('progress-bar sa1');		m2.removeClass().addClass('progress-bar re1');
+				m3.removeClass().addClass('progress-bar ga1');		m4.removeClass().addClass('progress-bar ma1');
+				m5.removeClass().addClass('progress-bar pa1');		m6.removeClass().addClass('progress-bar dha1');
+				m17.removeClass().addClass('progress-bar ni');
+			}
+			
+			else if(mk=="7")
+			{
+				m1.removeClass().addClass('progress-bar sa1');		m2.removeClass().addClass('progress-bar re1');
+				m3.removeClass().addClass('progress-bar ga1');		m4.removeClass().addClass('progress-bar ma1');
+				m5.removeClass().addClass('progress-bar pa1');		m6.removeClass().addClass('progress-bar dha1');
+				m7.removeClass().addClass('progress-bar ni1');
+			}
+			
+			else if(mk=="0")
+			{
+				m11.removeClass().addClass('progress-bar sa');		m12.removeClass().addClass('progress-bar re');
+				m13.removeClass().addClass('progress-bar ga');		m14.removeClass().addClass('progress-bar ma');
+				m15.removeClass().addClass('progress-bar pa');		m16.removeClass().addClass('progress-bar dha');
+				m17.removeClass().addClass('progress-bar ni');
+			}
+			
+		}
---
+++ assets/js/mkset2.js
@@ -0,0 +1,77 @@
+﻿	function setmk2()
+		{
+			var mks=document.getElementById("marks2").value;
+			console.log("mks = "+mks);
+			document.getElementById("mk2").innerHTML=mks;
+						
+			var z1 = $('.e1') ;		var z2 = $('.e2') ;		var z3 = $('.e3') ;
+			var z4 = $('.e4') ;		var z5 = $('.e5') ;		var z6 = $('.e6') ;		var z7 = $('.e7') ;			
+			
+			var z11 = $('.e11') ;	var z12 = $('.e12') ;	var z13 = $('.e13') ;
+			var z14 = $('.e14') ;	var z15 = $('.e15') ;	var z16 = $('.e16') ;	var z17 = $('.e17') ;			
+			
+			if(mks=="1")
+			{
+				z1.removeClass().addClass('progress-bar e11');		z12.removeClass().addClass('progress-bar e2');
+				z13.removeClass().addClass('progress-bar e3');		z14.removeClass().addClass('progress-bar e4');
+				z15.removeClass().addClass('progress-bar e5');		z16.removeClass().addClass('progress-bar e6');
+				z17.removeClass().addClass('progress-bar e7');
+			}			
+			
+			else if(mks=="2")
+			{
+				z1.removeClass().addClass('progress-bar e11');		z2.removeClass().addClass('progress-bar e12');
+				z13.removeClass().addClass('progress-bar e3');		z14.removeClass().addClass('progress-bar e4');
+				z15.removeClass().addClass('progress-bar e5');		z16.removeClass().addClass('progress-bar e6');
+				z17.removeClass().addClass('progress-bar e7');
+			}
+						
+			else if(mks=="3")
+			{
+				z1.removeClass().addClass('progress-bar e11');		z2.removeClass().addClass('progress-bar e12');
+				z3.removeClass().addClass('progress-bar e13');		z14.removeClass().addClass('progress-bar e4');
+				z15.removeClass().addClass('progress-bar e5');		z16.removeClass().addClass('progress-bar e6');
+				z17.removeClass().addClass('progress-bar e7');
+			}
+									
+			else if(mks=="4")
+			{
+				z1.removeClass().addClass('progress-bar e11');		z2.removeClass().addClass('progress-bar e12');
+				z3.removeClass().addClass('progress-bar e13');		z4.removeClass().addClass('progress-bar e14');
+				z15.removeClass().addClass('progress-bar e5');		z16.removeClass().addClass('progress-bar e6');
+				z17.removeClass().addClass('progress-bar e7');
+			}			
+			
+			else if(mks=="5")
+			{
+				z1.removeClass().addClass('progress-bar e11');		z2.removeClass().addClass('progress-bar e12');
+				z3.removeClass().addClass('progress-bar e13');		z4.removeClass().addClass('progress-bar e14');
+				z5.removeClass().addClass('progress-bar e15');		z16.removeClass().addClass('progress-bar e6');
+				z17.removeClass().addClass('progress-bar e7');
+			}
+			
+			else if(mks=="6")
+			{
+				z1.removeClass().addClass('progress-bar e11');		z2.removeClass().addClass('progress-bar e12');
+				z3.removeClass().addClass('progress-bar e13');		z4.removeClass().addClass('progress-bar e14');
+				z5.removeClass().addClass('progress-bar e15');		z6.removeClass().addClass('progress-bar e16');
+				z17.removeClass().addClass('progress-bar e7');
+			}
+			
+			else if(mks=="7")
+			{
+				z1.removeClass().addClass('progress-bar e11');		z2.removeClass().addClass('progress-bar e12');
+				z3.removeClass().addClass('progress-bar e13');		z4.removeClass().addClass('progress-bar e14');
+				z5.removeClass().addClass('progress-bar e15');		z6.removeClass().addClass('progress-bar e16');
+				z7.removeClass().addClass('progress-bar e17');
+			}
+			
+			else if(mks=="0")
+			{
+				z11.removeClass().addClass('progress-bar e1');		z12.removeClass().addClass('progress-bar e2');
+				z13.removeClass().addClass('progress-bar e3');		z14.removeClass().addClass('progress-bar e4');
+				z15.removeClass().addClass('progress-bar e5');		z16.removeClass().addClass('progress-bar e6');
+				z17.removeClass().addClass('progress-bar e7');
+			}
+			
+		}
